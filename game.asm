3279e39298dcdf90118e6f92fb8cebafe05d0003
Ryan O'Dell <ryanodell2013@gmail.com>
2025-05-09 12:01:20 -0500

    Added ability to print a number

--- game.asm
+++ game.asm
@@ -2,7 +2,7 @@
 %include "macros.inc"
 
 section .data
-	message		db 	"Hello"
+	message		db 	"Hello", 10
 	message_len	equ	$-message
 
 
@@ -12,5 +12,47 @@ section .text
 _start:
 	; mov rax, message
 	print message
-
+	
+	mov rax, 123
+	call _printRAX	
 	exit
+
+_printRAX:
+	mov rcx, digitSpace
+	mov rbx, 10
+	mov [rcx], rbx
+	inc rcx
+	mov [digitSpacePos], rcx
+
+_printRAXLoop:
+	mov rdx, 0
+	mov rbx, 10
+	div rbx				; divide so we can get the remainder
+	push rax
+	add rdx, 48			; Needed to convert it to ASCII
+
+	mov rcx, [digitSpacePos]
+	mov [rcx], dl			; dl = lower 8 bytes of rdx?
+	inc rcx
+	mov [digitSpacePos], rcx
+
+	pop rax
+	cmp rax, 0			; Go until the remainder is 0
+	jne _printRAXLoop
+
+_printRAXLoop2:
+	mov rcx, [digitSpacePos]
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, rcx
+	mov rdx, 1
+	syscall
+
+	mov rcx, [digitSpacePos]
+	dec rcx
+	mov [digitSpacePos], rcx
+
+	cmp rcx, digitSpace
+	jge _printRAXLoop2
+
+	ret
